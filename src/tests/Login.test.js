6b7d91feb6864957fad89f9313825c72b5f45f5b
Marco Viana <marcoviana100@gmail.com>
2022-10-11 17:43:06 -0300

    req 4, 5, 6 e 7

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-/* import userEvent from '@testing-library/user-event'; */
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 import App from '../App';
@@ -17,8 +17,29 @@ describe('Verifique se a página inicial funciona corretamete', () => {
     expect(inputSenha).toBeInTheDocument();
     expect(buttonEntrar).toBeInTheDocument();
   });
+  test('Verifique a página de Login', async () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    const inputEmail = screen.getByPlaceholderText('email');
+    const inputSenha = screen.getByPlaceholderText('senha');
+    const { pathname } = history.location;
+    const buttonEntrar = screen.getByRole('button', { name: /entrar/i });
 
-  test('Verifica se a /carteira tudo funciona corretamente', () => {
+    const regexEmail1 = /\S+@\S+\.\S+/;
+    const emailTest = regexEmail1.test(inputEmail.value);
+    expect(emailTest).toBe(false);
+    expect(buttonEntrar.type).toBe('button');
+    userEvent.type(inputEmail, 'example@example.com');
+    userEvent.type(inputSenha, '100Marco');
+
+    expect(buttonEntrar).toBeEnabled();
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputSenha).toBeInTheDocument();
+    expect(inputSenha.value.length).toEqual(8);
+    expect(inputEmail.value).toBe('example@example.com');
+    expect(pathname).toBe('/');
+  });
+
+  test('Verifica se a /carteira renderiza os elementos necessários', () => {
     const { history } = renderWithRouterAndRedux(<App />);
     act(() => {
       history.push('/carteira');
@@ -38,5 +59,54 @@ describe('Verifique se a página inicial funciona corretamete', () => {
     expect(emailGravado).toBeInTheDocument();
     expect(valorTotal).toBeInTheDocument();
     expect(moeda).toBeInTheDocument();
+  });
+  test('se os elementos da /carteira funcionam corretamente', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    act(() => {
+      history.push('/carteira');
+    });
+    const inputValue = screen.getByPlaceholderText(/valor da despesa/i);
+    const description = screen.getByPlaceholderText(/descrição da despesa/i);
+    const moeda = screen.getByText('Moeda');
+    userEvent.type(inputValue, '350');
+    userEvent.type(description, 'bife à parmegiana');
+
+    const metodoPagamento = screen.getByText('Método de pagamento');
+
+    const category = screen.getByText('Tag');
+
+    const adicionarDespesa = screen.getByRole('button', { name: /adicionar despesa/i });
+    userEvent.click(adicionarDespesa);
+    expect(moeda).toBeInTheDocument();
+    expect(metodoPagamento).toBeInTheDocument();
+    expect(category).toBeInTheDocument();
+    expect(inputValue.value).toBe('');
+    expect(inputValue.value.length).toEqual(0);
+    expect(description.value).toBe('');
+  });
+
+  test('Verifique se o select funciona corretamente', async () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+
+    act(() => {
+      history.push('/carteira');
+    });
+
+    const moeda = screen.getByTestId('currency-input');
+    expect(moeda).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(moeda).toHaveValue('USD');
+    });
+
+    const methodPay = screen.getByTestId('method-input');
+    userEvent.selectOptions(methodPay, 'Cartão de crédito');
+    expect(methodPay).toHaveValue('Cartão de crédito');
+
+    userEvent.selectOptions(moeda, 'ETH');
+    expect(moeda).toHaveValue('ETH');
+
+    const adicionarDespesa = screen.getByRole('button', { name: /adicionar despesa/i });
+    userEvent.click(adicionarDespesa);
   });
 });
